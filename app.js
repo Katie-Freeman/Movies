456d3c5f5d5b279b2c9da5cc3aa9813de93a60b3
Katie Freeman <katiefreeman@carltons-mbp.lan>
2022-01-27 22:12:00 -0500

    add login and authintication to movies app

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const app = express()
 const bodyParser = require("body-parser")
 const mustacheExpress = require('mustache-express')
 const moviesRouter = require('./routes/movies')
+const session = require('express-session')
 
- 
+app.use(session({
+    secret: 'Super secret words',
+    resave: false,
+    saveUninitalized: true
+  }))
 
 app.engine('mustache', mustacheExpress('./views' + '/partials', '.mustache'))
 app.set('views', './views')
@@ -12,18 +17,92 @@ app.set('views', './views')
 app.use(bodyParser.urlencoded({extended: false}))
 app.use('/css', express.static("css")) 
 app.use('/movies', moviesRouter)
-app.use('/', moviesRouter)
 app.set('view engine', 'mustache')
 
 
 
-global.movies = [
-]
+global.movies = []
+global.users = []
 
-app.get('/api/movies', (req, res) => {
-    res.json(movies)
+function logMiddleware(req, res, next) {
+    console.log('MIDDLEWARE')
+    next() // continue with the original request 
+}
+
+ function authenticateMiddleware(req, res, next) {
+
+    if(req.session) {
+        if(req.session.username) {
+            next() 
+        } else {
+            res.redirect('/')
+        }
+    } else {
+        res.redirect('/')
+    }
+
+}
+
+
+
+app.get('/api/users', (req, res) => {
+    res.json(users)
 })
+
+app.get('/profile', authenticateMiddleware ,(req, res) => {
+    res.send('PROFILE')
+})
+
+app.get('/dashboard', authenticateMiddleware, (req, res) => {
+    res.render('dashboard')
+})
+
+app.get('/add-user',(req,res) => {
+
+    res.render('add-user')
+  })
+
+ 
+  app.post('/add-user',(req,res) => {
+
+    let username = req.body.username
+    let password = req.body.password
+    let userId = users.length +1
+    console.log("Getting User name", {username, password})
+    users.push({username, password, userId})
+    
+    res.redirect('/')
+  })
+
+app.post('/login', (req, res) => {
+    
+    // get username from body 
+    const username = req.body.username 
+    // get password from body 
+    const password = req.body.password 
+
+    const persistedUser = users.find(user => {
+        return user.username == username && user.password == password
+    })
+
+    if(persistedUser) {
+         // if username and password are matching 
+        if(req.session) {
+            req.session.username = persistedUser.username 
+        }
+        res.redirect('/movies/add-movie')
+    } else {
+        res.render('index', {errorMessage: 'Username or password is invalid!'})
+    }
+})
+
+
+app.get('/',(req,res) => {
+    res.render('index')
+  })
+
 
 app.listen(3000, () => {
     console.log('Server is running...')
 })
+
--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,36 @@
 
 const express = require('express')
 const router = express.Router()
+const session = require('express-session')
 
+router.use(session({
+    secret: 'Super secret words',
+    resave: false,
+    saveUninitalized: true
+  }))
 
+function logMiddleware(req, res, next) {
+    console.log('MIDDLEWARE')
+    next()  
+}
 
+ function authenticateMiddleware(req, res, next) {
 
-router.get('/add-movie', (req, res) => {
+    if(req.session) {
+        if(req.session.username) {
+            next() 
+        } else {
+            res.redirect('/')
+        }
+    } else {
+        res.redirect('/')
+    }
+
+}
+
+movies = []
+
+router.get('/add-movie', authenticateMiddleware, (req, res) => {
     res.render('add-movie')
 })
 
@@ -13,26 +38,27 @@ router.get('/api/movies', (req, res) => {
     res.json(movies)
 })
 
-router.post('/add-movie', (req, res) => {
+router.post('/add-movie',(req, res) => {
     let{title, description, genre, image} = req.body
     const movie = {movieId: movies.length +1, title: title, description: description, genre: genre, image: image}
     
     movies.push(movie)
+    console.log(movie)
     res.redirect('/movies')
 })
 
-router.get('/', (req, res) => {
+router.get ('/', authenticateMiddleware, (req, res) => {
     res.render('movies', {allMovies: movies})
 })
 
-router.post('/delete-movie', (req, res) => {
+router.post('/delete-movie',  (req, res) => {
     const movieId = req.query.movieId
     console.log("DELETING")
     movies = movies.filter(movie => movie.movieId != movieId)
     res.redirect('/movies')
 })
 
-router.get('/update-movie', (req, res) => {
+router.get('/update-movie',  authenticateMiddleware, (req, res) => {
     const movieId=req.query.movieId
     console.log("getting movieId", movieId)
     const movie = movies.filter(movie => movie.movieId == movieId)[0]
